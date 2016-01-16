9dcb1120099aae09207ce42b7c339c55e81bcdd2
parentheses <parentheses@bitbucket.org>
2016-01-16 12:23:53 -0800

    add api controller, twitter service class

---
+++ wehack/Controllers/ComplaintApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace wehack.Controllers
+{
+    [RoutePrefix("api/complaint")]
+    public class ComplaintApiController : ApiController
+    {
+        [Route("send")]
+        [HttpPost]
+        public HttpResponseMessage SendComplaint(/*ComplaintModel model*/)
+        {
+            HttpResponseMessage resp = null;
+            try 
+            {
+            
+            }
+            catch (Exception ex)
+            {
+                resp = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+
+            return resp;
+        }
+    }
+}
